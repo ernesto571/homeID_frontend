8326747e7033b44277e86116045b9f7526941c0c
ernesto571 <emmanueladesemoye@gmail.com>
2026-03-24 10:41:55 +0100

    JS obfuscator implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    obfuscatorPlugin({
+      options: {
+        rotateStringArray: true,
+        stringArray: true,
+        stringArrayThreshold: 0.75,
+        deadCodeInjection: false,
+        selfDefending: false,
+        debugProtection: false,
       },
     }),
   ],
